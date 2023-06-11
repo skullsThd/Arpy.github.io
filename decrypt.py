e4504a4a830dbd263c4473f01a12772f58a99e37
skullsThd <104901809+skullsThd@users.noreply.github.com>
2023-06-11 12:19:19 +0200

    Update decrypt.py

--- decrypt.py
+++ decrypt.py
@@ -1,27 +1,26 @@
 #!/usr/bin/env python3
 
-import os 
+import os
 from cryptography.fernet import Fernet
 
-#let's find some files
-
+# Let's find some files
 
 files = []
 
 for file in os.listdir():
-       if file == "v3voost.py" or file == "theKey.key" or file == "decrypt.py":
-              continue 
-       if  os.path.isfile(file):
-             files.append(file)
+    if file == "v3voost.py" or file == "theKey.key" or file == "decrypt.py":
+        continue
+    if os.path.isfile(file):
+        files.append(file)
 
-with open("thenKey.key", "rb") as key:
-        secretkey = key.read()
+with open("theKey.key", "rb") as key:
+    secretkey = key.read()
 
 print(files)
 
 for file in files:
-         with open(file, "rb") as thefile:
-                  contents = thefile.read()
-                  contents_decrypted = Fernet(secretkey).decrypt(contents)
-                  with open(file, "wb") as thefile:
-                         thefile.write(contents_decrypted)
+    with open(file, "rb") as thefile:
+        contents = thefile.read()
+        contents_decrypted = Fernet(secretkey).decrypt(contents)
+        with open(file, "wb") as thefile:
+            thefile.write(contents_decrypted)
